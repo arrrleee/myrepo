eac40217b29eb543ae35f7bb3fdd0420914ed787
Randy Lee <randy.wd.lee@gmail.com>
2014-01-20 13:44:03 +0000

    made fixes

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8080);
-=======
 // server.js
 var express = require('express');
 var fs = require('fs');
@@ -25,4 +16,3 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
->>>>>>> master
