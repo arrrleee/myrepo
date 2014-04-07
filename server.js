f42b0ae5e4696e6f2cb981fa0724c8cbab63dff5
Randy Lee <randy.wd.lee@gmail.com>
2014-04-07 23:49:07 +0000

    added vibes.html and added it to server.js

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 // server.js
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
+var url = require('url');
+
+var indexhtmlfile = "index.html";
+var vibeshtmlfile = "vibes.html";
+var htmlfile;
 
 var app = express.createServer(express.logger());
+//var app = http.createServer( function(req,res){
+//  var pathname = url.parse(req.url).pathname;
+//  switch(pathname){
+//    case '/vibes':
+//       res.end('vibeshtmlfile');
+//        break;
+//    default:
+//        res.end('indexhtmlfile');
+//        break;
+//  }
+//}).listen(8080);
+
 
 //app.use(logfmt.requestLogger());
 
 app.get('/', function(request, response) {
+  var path = url.parse(request.url).pathname;
+  switch(path) {
+    case '/vibes':
+      htmlfile = vibeshtmlfile;
+      break;
+    default:
+      htmlfile = indexhtmlfile;
+      break;
+  }
   var html = fs.readFileSync(htmlfile).toString();
   response.send(html);
 });
